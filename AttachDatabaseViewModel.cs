4104157ee4a77deb7272f3ee2dfe3c62079b0ee1
Alen Pelin <alienlab.com@gmail.com>
2014-10-04 08:55:26 +1000

    Implemented OpenFileDialog for cases when there is no command line argument

--- AttachDatabaseViewModel.cs
+++ AttachDatabaseViewModel.cs
@@ -3,11 +3,14 @@
   using System;
   using System.Collections.Specialized;
   using System.Dynamic;
+  using System.IO;
   using System.Linq;
   using System.Windows.Input;
+  using Microsoft.Win32;
 
   public class AttachDatabaseViewModel
   {
+    private string physicalPath;
     public DataSourceViewModel DataSourceViewModel { get; set; }
 
     public ICommand DoAttach { get; set; }
@@ -18,8 +21,26 @@
     {
       get
       {
-        return Environment.GetCommandLineArgs().Skip(1).FirstOrDefault();
+        return this.physicalPath ?? Environment.GetCommandLineArgs().Skip(1).FirstOrDefault() ?? (this.physicalPath = this.ChooseDatabaseFile());
       }
+    }
+
+    private string ChooseDatabaseFile()
+    {
+      var openFileDialog = new OpenFileDialog
+      {
+        AddExtension = true,
+        CheckFileExists = true,
+        CheckPathExists = true,
+        Filter = "MS SQL Server Database File (*.MDF)|*.mdf",
+        InitialDirectory = Path.GetPathRoot(Environment.ExpandEnvironmentVariables(Environment.SystemDirectory)),
+        Multiselect = false, // we do not support multiple mdf at once for now
+        Title = "Choose MS SQL Server Database .MDF File"
+      };
+
+      var result = openFileDialog.ShowDialog();
+      
+      return result.HasValue && result.Value ? openFileDialog.FileName : null;
     }
   }
 }
