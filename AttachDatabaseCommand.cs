6f023b8d4b1153eb9fe8a0f4a5cbb429014dc36a
Alen Pelin <alienlab.com@gmail.com>
2014-10-04 22:49:13 +1000

    Refactoring

--- AttachDatabaseCommand.cs
+++ AttachDatabaseCommand.cs
@@ -38,15 +38,53 @@
       var server = new Server(connection);
       try
       {
-        var serviceAccount = new SecurityIdentifier(server.Properties["ServiceAccountSid"].Value as byte[], 0);
+        var physicalPath = viewModel.PhysicalPath;
+        Assert.IsNotNull(physicalPath, "physicalPath");
+
+        var serviceAccount = GetServiceAccount(server);
         Assert.IsNotNull(serviceAccount, "serviceAccount");
-        SecurityHelper.EnsureFilePermissions(viewModel.PhysicalPath, serviceAccount);
-        server.AttachDatabase(viewModel.LogicalName, new StringCollection { viewModel.PhysicalPath });
+
+        try
+        {
+          SecurityHelper.EnsureFilePermissions(physicalPath, serviceAccount);
+        }
+        catch (Exception ex)
+        {
+          this.HandleError("Cannot ensure security permissions, but will try to attach anyway. ", ex);
+        }
+
+        try
+        {
+          server.AttachDatabase(viewModel.LogicalName, new StringCollection { physicalPath });
+          Application.Current.MainWindow.Close();
+        }
+        catch (Exception ex)
+        {
+          this.HandleError("Cannot attach the database. ", ex);
+        }
       }
       catch (Exception ex)
       {
-        MessageBox.Show("An error ocurred. " + PrintError(ex), "Database Management Tool", MessageBoxButton.OK, MessageBoxImage.Error);
+        this.HandleError("Cannot retrieve SQL server metadata. ", ex);
       }
+    }
+
+    private void HandleError(string message, Exception ex)
+    {
+      MessageBox.Show(message + this.PrintError(ex), "Database Management Tool", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+
+    private static SecurityIdentifier GetServiceAccount(Server server)
+    {
+      var serviceAccountSidProperty = server.Properties["ServiceAccountSid"];
+      Assert.IsNotNull(serviceAccountSidProperty, "The serviceAccountSid property is null");
+
+      var serviceAccountSidBytes = serviceAccountSidProperty.Value as byte[];
+      Assert.IsNotNull(serviceAccountSidBytes, "serviceAccountSidBytes");
+
+      var serviceAccount = new SecurityIdentifier(serviceAccountSidBytes, 0);
+
+      return serviceAccount;
     }
 
     private static SqlConnectionStringBuilder GetConnectionString(AttachDatabaseViewModel viewModel)
