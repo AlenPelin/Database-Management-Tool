cf486e7d8510106c7c3b41570713e9527e6f2cf2
Alen Pelin <alienlab.com@gmail.com>
2014-10-05 09:43:05 +1100

    Uninstall script is fixed

--- DoUninstall.bat
+++ DoUninstall.bat
@@ -1,6 +1,6 @@
 @ECHO off
 
-REG DELETE "hkcu\Software\Classes\SQLServer.Engine.PrimaryDataFile\shell\Open with scla" /f
+REG DELETE "hkcu\Software\Classes\SQLServer.Engine.PrimaryDataFile\shell\Attach Database" /f
 
 ECHO.
 ECHO Context menu handlers were deleted successfully.
